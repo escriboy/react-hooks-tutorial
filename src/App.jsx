acff3cda00edb6afc9a506ca28d6e7dbf7639186
YM CHOI <escriboy@gmail.com>
2023-01-11 20:58:50 +0900

    0111 useEffect

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import Info from "./components/Info";
+
+export default function App() {
+  return <Counter />;
+}
